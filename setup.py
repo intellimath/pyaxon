b4be3a626aa4df01f9584282ba0e81e4d7ef1495
zaur shibzukhov <aintellimath@gmail.com>
2013-07-25 09:54:58 +0400

    Set version

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ long_description = open('README.rst').read()
 
 setup(
     name = 'pyaxon',
-    version = '0.5.3',
+    version = '0.5.4',
     description = 'Python library for An eXtended Object Notation (AXON)',
     author = 'Zaur Shibzukhov',
     author_email="szport@gmail.com",
