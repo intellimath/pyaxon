0a8b4f40d8024c11a06f4edfb41a78e61ace197b
zaur shibzukhov <szport@gmail.com>
2015-08-10 22:23:40 +0300

    Refactor code for xml2axon script

--- lib/axon/utils.py
+++ lib/axon/utils.py
@@ -28,3 +28,35 @@ from axon._objects import undef, as_unicode, as_list, as_dict, as_tuple, as_name
 from axon._dumper import reduce, dump_as_str
 
 
+import axon
+import xml.etree.ElementTree as etree
+
+@reduce(etree.Element)
+def reduce_ElementTree(e):
+	attrs = {axon.as_unicode(name):val for name,val in e.attrib.items()}
+	if e.text:
+		vals = [e.text]
+	else:
+		vals = []
+	for child in e.getchildren():
+		vals.append(child)
+		if child.tail:
+			vals.append(child.tail)
+			child.tail = None
+	if len(attrs) == 0:
+		attrs = None
+	if len(vals) == 0:
+		vals = None
+	return axon.node(axon.as_unicode(e.tag), attrs, vals)
+    
+del reduce_ElementTree
+	
+
+def xml2axon(from_file, to_file='<string>'):
+    tree = etree.parse(from_file)
+    root = tree._root
+    if to_file == '<string>':
+        return axon.dumps([root], pretty=1)
+    else:
+        axon.dump(to_file, [root], pretty=1)
+
