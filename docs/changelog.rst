1237e8773f6daedecdf1980e352bff7546f63576
zaur shibzukhov <szport@gmail.com>
2015-08-27 13:39:58 +0300

    Add GA tracking to some pages

--- docs/changelog.rst
+++ docs/changelog.rst
@@ -130,3 +130,15 @@ Special credit to `sbant <https://bitbucket.org/sbant1983>`_.
 **0.5**
 
    First public release of ``pyaxon``.
+
+.. raw:: html
+
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-42353741-1', 'bitbucket.org');
+      ga('send', 'pageview');
+    </script>
--- docs/conf.py
+++ docs/conf.py
@@ -161,7 +161,7 @@ html_show_sphinx = False
 #html_use_opensearch = ''
 
 # This is the file name suffix for HTML files (e.g. ".xhtml").
-#html_file_suffix = None
+html_file_suffix = ".html"
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'axondoc'
@@ -170,15 +170,15 @@ htmlhelp_basename = 'axondoc'
 # -- Options for LaTeX output --------------------------------------------------
 
 # The paper size ('letter' or 'a4').
-#latex_paper_size = 'letter'
+#latex_paper_size = 'a4'
 
 # The font size ('10pt', '11pt' or '12pt').
-#latex_font_size = '10pt'
+latex_font_size = '12pt'
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 #latex_documents = [
-#  ('index', 'simpleon.tex', u'SimpleON Documentation',
+#  ('index', 'axon.tex', u'AXON Documentation',
 #   u'Shibzukhov Zaur', 'manual'),
 #]
 
